ea4e3b7f6ba40fa4ff58c8a7ca3a9893cc82112e
Elaine Cao <47513241+elainecao93@users.noreply.github.com>
2019-05-16 13:45:04 -0500

    removed print statements

--- post.py
+++ post.py
@@ -26,17 +26,11 @@ def makeSig(consumer_key, oauth_token, consumer_secret, token_secret, message, t
         first = False
         param_string += urllib.parse.quote(k) + "=" + urllib.parse.quote(auth_key[k])
     print(param_string)
-    #print(solution[0])
-    #print(param_string == solution[0])
 
     base_string = "POST&" + urllib.parse.quote(URL).replace("/", "%2F") + "&" + urllib.parse.quote(param_string)
     print(base_string)
-    #print(solution[1])
-    #print(base_string == solution[1])
     secret_keys = consumer_secret + "&" + token_secret
     print(secret_keys)
-    #print(solution[2])
-    #print(secret_keys == solution[2])
 
     key = bytes(secret_keys, "UTF-8")
     message = bytes(base_string, "UTF-8")
@@ -47,11 +41,9 @@ def makeSig(consumer_key, oauth_token, consumer_secret, token_secret, message, t
     print(signature)
 
     return signature
-    #print(solution[3])
 
 def makeAuth(consumer_key, oauth_token, consumer_secret, token_secret, message, timestamp, nonce, URL):
     signature = makeSig(consumer_key, oauth_token, consumer_secret, token_secret, message, timestamp, nonce, URL)
-    #signature = "tnnArxj06cWHq44gCs1OSKk/jLY="
     auth_dict = {"oauth_consumer_key":consumer_key, "oauth_nonce":nonce, "oauth_signature":signature,
     "oauth_signature_method":"HMAC-SHA1", "oauth_timestamp":timestamp, "oauth_token":oauth_token, 
     "oauth_version":"1.0"}
@@ -90,29 +82,6 @@ def postToTwitter():
     r = requests.post(url = URL, data = data, headers = headers)
     print(r.request.headers)
     print(r.text)
-
-
-def main():
-    #these are the test cases provided by twitter, not real tokens. Real tokens are stored in a gitignore'd file.
-    """f = open("solution.txt")
-    solutions = f.read().split("\n")
-    consumer_key = "xvz1evFS4wEEPTGEFPHBog"
-    oauth_token = "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb"
-    consumer_secret = "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw"
-    token_secret = "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE"
-    timestamp = "1318622958"
-    nonce = "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg"
-    URL = "https://api.twitter.com/1.1/statuses/update.json"
-    message = "Hello Ladies + Gentlemen, a signed OAuth request!"
-    sig = makeSig(consumer_key, oauth_token, consumer_secret, token_secret, message, timestamp, nonce, URL)
-    print(sig)
-    print(sig == solutions[3])
-
-    gen = makeAuth(consumer_key, oauth_token, consumer_secret, token_secret, message, timestamp, nonce, URL)
-    print(gen)
-    print(gen == solutions[4])
-    """
-
 
 if __name__ =="__main__":
     postToTwitter()
