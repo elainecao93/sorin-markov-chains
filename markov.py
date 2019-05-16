c1be4b6499c2589634e682b5f330d34c3f528684
Elaine Cao <47513241+elainecao93@users.noreply.github.com>
2019-05-16 15:02:17 -0500

    refactored markov chain into class

--- markov.py
+++ markov.py
@@ -4,51 +4,61 @@ import string
 
 MAX_TWEET_LENGTH = 280
 
-#initialize outside functions
-words = []
-chain = {}
+class Chain():
+    lines = []
+    words = []
+    chain = {}
+    isSetup = False
 
-f = open("data.txt", "r")
-raw = f.read()
+    @staticmethod
+    def setup(): #TODO set up the chains
+        f = open("data.txt", "r")
+        Chain.lines = f.read().split("\n")
+        for line in Chain.lines:
+            line = line + " EOL"
+            Chain.words.append(line.split(" "))
 
-lines = raw.split("\n")
-
-for line in lines:
-    line = line + " EOL"
-    words.append(line.split(" "))
-        
-for line in words:
-    for i, key1 in enumerate(line):
-        if key1 == "EOL":
-            break
-        key2 = line[i+1]
-        if key2 == "EOL":
-            break
-        word = line[i+2]
-        if (key1, key2) in chain:
-            chain[(key1, key2)].append(word)
-        else:
-            chain[(key1, key2)] = [word]
-
-def markov():
-    randstart = random.randint(1, len(lines))
-    key = (words[randstart][0], words[randstart][1])
-    if "EOL" in key:
-        return markov()
-    output = key[0] + " " + key[1]
-    while True:
-        word = random.choice(chain[key])
-        if word == "EOL" or len(output) > 400:
-            break
-        output = output + " " + word
-        key = (key[1], word)
-    if output in lines:
-        return markov()
-    return output
+        for line in Chain.words:
+            for i, key1 in enumerate(line):
+                if key1 == "EOL":
+                    break
+                key2 = line[i+1]
+                if key2 == "EOL":
+                    break
+                word = line[i+2]
+                if (key1, key2) in Chain.chain:
+                    Chain.chain[(key1, key2)].append(word)
+                else:
+                    Chain.chain[(key1, key2)] = [word]
+    
+    def __init__(self):
+        if not Chain.isSetup:
+            Chain.setup()
+            Chain.isSetup = True
+    
+    def make(self):
+        randstart = random.randint(1, len(Chain.lines))
+        key = (Chain.words[randstart][0], Chain.words[randstart][1])
+        if "EOL" in key:
+            self.make()
+            return self.output
+        output = key[0] + " " + key[1]
+        while True:
+            word = random.choice(Chain.chain[key])
+            if word == "EOL" or len(output) > 400:
+                break
+            output = output + " " + word
+            key = (key[1], word)
+        if output in Chain.lines:
+            self.make()
+            return self.output
+        self.output = output
+        return output
 
 def main():
-    for _ in range(40):
-        print(markov())
+    test = Chain()
+    test.make()
+    print(test.output)
 
 if __name__ =="__main__":
     main()
